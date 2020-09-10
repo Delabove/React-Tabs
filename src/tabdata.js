fc4e4105e1f44a4d05e9b4ce6f25cdc8e6f1c637
DeLayne LaBove <delayne.labove@gmail.com>
2020-09-10 14:44:37 -0500

    basic project structure

---
+++ src/tabdata.js
@@ -0,0 +1,169 @@
+import React from 'react';
+
+const tabData = [
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Life',
+        title: 'Why We Traded The City for the Farm',
+
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Life',
+        title: 'Our Favorite Days on the Farm Look Like This',
+    },
+    {category: 'Favorite Reads',
+    subcategory: 'Farm Basics',
+    title: 'Grain Bin Safety',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Basics',
+        title: 'Soil pH',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Livestock',
+        title: 'Which Livestock is Most Profitable',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Livestock',
+        title: 'The Most Common Livestock For Your Farm',
+
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Seasonal Crops',
+        title: 'Dog Days of Summer',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Seasonal Crops',
+        title: 'Easiest Fall Crops',
+    },
+    {category: 'Favorite Reads',
+    subcategory: 'Farm Equipment',
+    title: 'Essential Equipment for Small Farms',
+    },
+    {
+        category: 'Favorite Reads',
+        subcategory: 'Farm Equipment',
+        title: 'Rotary Tillers: Why They Are Necessary',
+    },
+
+
+
+
+
+
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Life',
+        title: 'A Day On The Farm',
+
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Life',
+        title: 'My Favorite Season On The Farm',
+    },
+    {category: 'Most Talked About',
+    subcategory: 'Farm Basics',
+    title: 'How Much Water Do Farmers Manage',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Basics',
+        title: 'Nutrient Availability',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Livestock',
+        title: 'Which Livestock is Most Profitable',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Livestock',
+        title: 'The Healthiest Livestock To Raise For The Fridge',
+
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Seasonal Crops',
+        title: 'Interested In Growing Beets?',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Seasonal Crops',
+        title: 'Beans: A Warm Weather Favorite',
+    },
+    {category: 'Most Talked About',
+    subcategory: 'Farm Equipment',
+    title: 'Must Have Blades To Have On Hand At All Times',
+    },
+    {
+        category: 'Most Talked About',
+        subcategory: 'Farm Equipment',
+        title: 'Lawn Care Equipment',
+    },
+
+
+
+
+
+
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'How We Started Farming',
+
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Life',
+        title: 'The Main Reason I Love Farm Life',
+    },
+    {category: 'Most Shared',
+    subcategory: 'Farm Basics',
+    title: 'Grass Waterways',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Basics',
+        title: 'How Farmers Pick Their Planting Population',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Livestock',
+        title: 'The Processing of Home Grown Meat',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Livestock',
+        title: 'Dairy Cattle: The Good and The Bad',
+
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Seasonal Crops',
+        title: 'How the Heat Affects Crops',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Seasonal Crops',
+        title: 'Preparing For Winter',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Equipment',
+        title: 'Our Most Used Tractor',
+    },
+    {
+        category: 'Most Shared',
+        subcategory: 'Farm Equipment',
+        title: 'My Favorite Old Pickup Truck',
+    },
+  ];
+
+  export default tabData;
